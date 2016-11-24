175f36ff8eed80b3c2446d1b9b4c4d3900dee3b6
nshan.barseghyan <nshan.barseghyan@PC-0609>
2016-11-24 21:01:12 +0400

    S

--- ConfigSSIS/Program.cs
+++ ConfigSSIS/Program.cs
@@ -17,7 +17,13 @@ namespace ConfigSSIS
 
         static void Main(string[] args) // scan package in 3 ways
         {
-            myPackageAdd_PartnerIdSpec();
+            myPackageChange_Build_AllzTables_UpdateParams();
+            //myPackageChange_Build_AllzTables_Change();
+            //myPackageChange_Build_AllzPackages_List();
+            //myPackageDisable_ClearStaging();
+            //myPackageReplaceEdit_LogStart1();
+            //myPackageAdd_AddPackageConnection();
+            //myPackageAdd_PartnerIdDefaultUserPSWD();
             //myPackageChangeDoAllSchema();
             //myPackageWork();
             //myPackageReplaceEdit_LogStart();
@@ -32,6 +38,187 @@ namespace ConfigSSIS
             package = PackageUtils.LoadPackage("CheckSchema", null as Project);
             PackageUtils.ScanPackage(package);
             //PackageUtils.ScanMyTaskHostPackage(package, "LogStart");
+        }
+        static void myPackageChange_Build_AllzPackages_List()
+        {
+            Project project = Project.OpenProject(PackageUtils.projectPath);
+            List<SimpleDal.SyncTable> tables = SimpleDal.getSyncTables();
+            List<Package> zpackages = new List<Package>();
+            Package etalon = null;
+            foreach (var pi in project.PackageItems)
+            {
+                if (pi.Package.Name == "EmptyPlaceHolder")
+                {
+                    etalon = pi.Package;
+                    continue;
+                }
+                if (pi.Package.Name.Substring(0, 1) != "z") continue;
+                zpackages.Add(pi.Package);
+            }
+            Console.WriteLine("Synctable:{0}; Packages:{1}", tables.Count, zpackages.Count);
+            int exists = 0;
+            foreach (SimpleDal.SyncTable t in tables.OrderBy(x => x.Name))
+            {
+                Package p = zpackages.Where(x => (x.Name + ".dtsx").ToLower() == t.PackageName.ToLower()).SingleOrDefault();
+                if (p == null)
+                {
+                    Console.WriteLine("Synctable:{0}; Packages:{1}", t.Name, "missing!");
+                    exists++;
+                    continue;
+                }
+                if (!p.Name.Contains(t.Name))
+                {
+                    Console.WriteLine("Synctable:{0}; Packages:{1} {2}", t.Name, p.Name, "Wrong package name!");
+                    exists++;
+                    continue;
+                }
+            }
+            if (exists != 0)
+            {
+                Console.WriteLine("Error! Processing stopped");
+            }
+            zpackages.Add(etalon);
+            tables.Add(new SimpleDal.SyncTable() { Name = "EmptyPlaceHolder", PackageName = "EmptyPlaceHolder.dtsx" });
+            foreach (var t in tables.OrderBy(x => x.Name))
+            {
+                exists = 0;
+                //if (!t.FullJoin) continue;
+                Package p = zpackages.Where(x => (x.Name + ".dtsx").ToLower() == t.PackageName.ToLower()).SingleOrDefault();
+                foreach (Parameter par in p.Parameters)
+                {
+                    if (par.Name == "partnerSpec")
+                    {
+                        exists++;
+                        //p.Parameters.Remove("Delete_FriendlyExpression");
+                        break;
+                    }
+                }
+                //Variable variable = null;
+                //foreach (Variable v in p.Variables)
+                //{
+                //    if (v.Name == "destDelStagingTable")
+                //    {
+                //        variable = v;
+                //        exists++;
+                //        break;
+                //    }
+                //}
+                if (exists == 0)
+                {
+                    var par = p.Parameters.Add("partnerSpec", TypeCode.String);
+                    par.Value = "None";
+                    //p.Variables.Remove(variable);
+                    string pText;
+                    Console.WriteLine("changing : {0}", p.Name);
+                    p.SaveToXML(out pText, null);
+                    File.WriteAllText(destPath + p.Name + ".dtsx", pText);
+                }
+            }
+        }
+        static void myPackageChange_Build_AllzTables_UpdateParams()
+        {
+            Project project = Project.OpenProject(PackageUtils.projectPath);
+            List<SimpleDal.SyncTable> tables = SimpleDal.getSyncTables();
+            tables.Add(new SimpleDal.SyncTable() { Name = "EmptyPlaceHolder", PackageName = "EmptyPlaceHolder.dtsx" });
+            int exists;
+            int update;
+            Package p;
+            foreach (SimpleDal.SyncTable t in tables.OrderBy(x => x.Name))
+            {
+                p = project.PackageItems.Where(x => (x.Package.Name + ".dtsx").ToLower() == t.PackageName.ToLower()).SingleOrDefault().Package;
+                exists = 0;
+                update = 0;
+                foreach (Parameter par in p.Parameters)
+                {
+                    if (par.Name == "partnerSpec")
+                    {
+                        exists++;
+                        var s = (string)par.Value;
+                        if (s != t.PartnerSpec)
+                        {
+                            if (!(String.IsNullOrEmpty(t.PartnerSpec) && s == "None"))
+                            {
+                                par.Value = String.IsNullOrEmpty(t.PartnerSpec) ? "None" : t.PartnerSpec;
+                                update++;
+                            }
+                        }
+                    }
+                    if (par.Name == "SyncTableName")
+                    {
+                        exists++;
+                        var s = (string)par.Value;
+                        if (s != t.Name)
+                        {
+                            par.Value = t.Name;
+                            update++;
+                        }
+                    }
+                }
+                if (exists != 2)
+                {
+                    Console.WriteLine("package : {0}, missing parameter! : {1}", p.Name, exists);
+                    //Parameter par =  p.Parameters.Add("partnerSpec", TypeCode.String);
+                    //par.Value = String.IsNullOrEmpty(t.PartnerSpec) ? "None" : t.PartnerSpec;
+                }
+                if (update > 0)
+                {
+                    string pText;
+                    Console.WriteLine("package : {0}, changeN : {1}", p.Name, update);
+                    p.SaveToXML(out pText, null);
+                    File.WriteAllText(destPath + p.Name + ".dtsx", pText);
+                }
+            }
+        }
+
+        static void myPackageChange_Build_AllzTables_Change()
+        {
+            Project project = Project.OpenProject(PackageUtils.projectPath);
+            List<SimpleDal.SyncTable> tables = SimpleDal.getSyncTables();
+            tables.Add(new SimpleDal.SyncTable() { Name = "EmptyPlaceHolder", PackageName = "EmptyPlaceHolder.dtsx" });
+            foreach (SimpleDal.SyncTable t in tables.OrderBy(x => x.Name))
+            {
+                Package p = project.PackageItems.Where(x => (x.Package.Name + ".dtsx").ToLower() == t.PackageName.ToLower()).SingleOrDefault().Package;
+                TaskHost th;
+                Variable variable = null;
+                int update = 0;
+                Executable executable = null;
+                foreach (Executable e in p.Executables)
+                {
+                    th = e as TaskHost;
+                    if (th == null)
+                    {
+                        //Console.WriteLine("type : {0}", e.GetType()); // Microsoft.SqlServer.Dts.Runtime.Sequence; Microsoft.SqlServer.Dts.Runtime.ForEachLoop
+                        continue;
+                    }
+                    if (th.Name == "GetLastKey" && th.CreationName.Contains("ExecuteSQLTask"))
+                    {
+                        Console.WriteLine("package : {0}", p.Name);
+                        //foreach (Variable v in p.Variables)
+                        //{
+                        //    if (v.Name == "syncLogName")
+                        //    {
+                        //        variable = v;
+                        //        executable = e;
+                        //        update++;
+                        //        break;
+                        //    }
+                        //}
+                    }
+                }
+                if (update == 1)
+                {
+                    variable.EvaluateAsExpression = true;
+                    variable.Expression = "\"Sync \"+@[$Package::SyncTableName]+\" \"+ REPLACE( @[System::PackageName], \"z\" + @[$Package::SyncTableName]+\"_\", \"\" )";
+                    p.Executables.Remove(executable);
+                    string pText;
+                    Console.WriteLine("changing : {0}", p.Name);
+                    p.SaveToXML(out pText, null);
+                    File.WriteAllText(destPath + p.Name + ".dtsx", pText);
+                    continue;
+                }
+                if (update > 0) Console.WriteLine("!!!{0}!!!", p.Name);
+            }
+
         }
         static void myPackageChangeDoAllSchema()
         {
@@ -48,7 +235,67 @@ namespace ConfigSSIS
             package.SaveToXML(out pText, null);
             File.WriteAllText(destFullPath, pText);
         }
-        static void myPackageAdd_PartnerIdSpec()
+        static void myPackageAdd_AddPackageConnection()
+        {
+            string pText;
+            Project project = Project.OpenProject(PackageUtils.projectPath);
+            // get Connection Example
+            Package packageEx = PackageUtils.LoadPackage("CheckSchema", project);
+            ConnectionManager conn = null;
+            foreach (ConnectionManager par in packageEx.Connections)
+            {
+                if (par.Name == "PdestConnmgr")
+                {
+                    conn = par;
+                    break;
+                }
+            }
+            packageEx.Connections.Remove(conn);
+            if (conn == null)
+            {
+                Console.WriteLine("Example not found!");
+                return;
+            }
+            foreach (var pi in project.PackageItems)
+            {
+                Package packageS = pi.Package;
+                if (packageS.Name == "CheckSchema") continue;
+                int exists = 0;
+                // check if change is needed
+                foreach (Parameter par in packageS.Parameters)
+                {
+                    if (par.Name == "dPSWD" && (string)par.Value == "1") exists++;
+                    if (par.Name == "dUser" && (string)par.Value == "SSIS_partnersync") exists++;
+                }
+                if (exists != 2)
+                {
+                    Console.WriteLine("NF {0} Skipped:{1}", packageS.Name, exists);
+                    continue;
+                }
+                exists = 0;
+                foreach (ConnectionManager par in packageS.Connections)
+                {
+                    if (par.Name == "PdestConnmgr")
+                    {
+                        exists++;
+                        break;
+                    }
+                }
+                if (exists == 1)
+                {
+                    Console.WriteLine("FF {0} Skipped:{1}", packageS.Name, exists);
+                    continue;
+                }
+
+                Console.WriteLine("Changing {0}", packageS.Name);
+                packageS.Connections.Join(conn);
+
+                packageS.SaveToXML(out pText, null);
+                File.WriteAllText(destPath + packageS.Name + ".dtsx", pText);
+                packageS.Connections.Remove(conn);
+            }
+        }
+        static void myPackageAdd_PartnerIdDefaultUserPSWD()
         {
             Project project = Project.OpenProject(PackageUtils.projectPath);
             string pText;
@@ -58,39 +305,113 @@ namespace ConfigSSIS
                 int exists = 0;
                 foreach (Parameter par in packageS.Parameters)
                 {
-                    if (par.Name == "PartnerId")
+                    if (par.Name == "dPSWD")
                     {
-                        if (par.DataType == TypeCode.Int32 || (int)par.Value == -2)
+                        if ((string)par.Value != "1")
                         {
                             exists++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("!!! Package:{0} - Name:{1} - DataType:{2} - Value:{3}", packageS.Name, par.Name, par.DataType, par.Value);
+                            par.Value = "1";
+                            Console.WriteLine("pswd! Package:{0} - Name:{1} - DataType:{2} - Value:{3}", packageS.Name, par.Name, par.DataType, par.Value);
+                            continue;
                         }
                     }
-                    if (par.Name == "dCatalogName")
+                    if (par.Name == "dUser")
                     {
-                        Console.WriteLine("!!! Package:{0} - Name:{1} - DataType:{2} - Value:{3}", packageS.Name, par.Name, par.DataType, par.Value);
+                        if ((string)par.Value != "SSIS_partnersync")
+                        {
+                            exists++;
+                            par.Value = "1";
+                            Console.WriteLine("user! Package:{0} - Name:{1} - DataType:{2} - Value:{3}", packageS.Name, par.Name, par.DataType, par.Value);
+                            continue;
+                        }
+                    }
+                }
+                if (exists > 0)
+                {
+                    //Parameter par = packageS.Parameters.Add("dCatalogName", TypeCode.String);
+                    //par.Value = "SBdest";
+                    packageS.SaveToXML(out pText, null);
+                    File.WriteAllText(destPath + packageS.Name + ".dtsx", pText);
+                }
+
+            }
+        }
+        static void myPackageDisable_ClearStaging()
+        {
+            string pText;
+            Project project = Project.OpenProject(PackageUtils.projectPath);
+            string taskHostName = "Clear Staging";
+            Package p;
+            TaskHost th, thCh = null;
+            Variable var = null;
+            foreach (PackageItem pItem in project.PackageItems)
+            {
+                p = pItem.Package;
+                int exists = 0;
+                foreach (Executable e in p.Executables)
+                {
+                    th = e as TaskHost;
+                    //Console.WriteLine("Package:{0}", th==null?"-":th.Name);
+
+                    if (th != null && th.Name == taskHostName)
+                    {
                         exists++;
+                        var = p.Variables.Cast<Variable>().FirstOrDefault(x => x.Name == "destDelStagingTable");
+                        if (var != null)
+                        {
+                            thCh = th;
+                            exists++;
+                        }
                     }
                 }
                 if (exists == 2)
                 {
-
+                    Console.WriteLine("{1} Package:{0}", p.Name, exists);
+                    thCh.Disable = true;
+                    p.SaveToXML(out pText, null);
+                    File.WriteAllText(destPath + p.Name + ".dtsx", pText);
                 }
-                if (exists == 1)
+            }
+        }
+        static void myPackageReplaceEdit_LogStart1()
+        {
+            string pText;
+            Project project = Project.OpenProject(PackageUtils.projectPath);
+            string taskHostName = "LogStart";
+            Package p;
+            TaskHost th;
+            Variable var = null;
+            foreach (PackageItem pItem in project.PackageItems)
+            {
+                p = pItem.Package;
+                int exists = 0;
+                foreach (Executable e in p.Executables)
                 {
-                    Parameter par = packageS.Parameters.Add("dCatalogName", TypeCode.String);
-                    par.Value = "SBdest";
-                    par = packageS.Parameters.Add("dDataSource", TypeCode.String);
-                    par.Value = "PC-0609";
-                    par = packageS.Parameters.Add("dUser", TypeCode.String);
-                    par.Value = "SSIS_partnersync";
-                    par = packageS.Parameters.Add("dPSWD", TypeCode.String);
-                    par.Value = "dPSWD";
-                    packageS.SaveToXML(out pText, null);
-                    File.WriteAllText(destPath + packageS.Name + ".dtsx", pText);
+                    th = e as TaskHost;
+                    if (th != null && th.Name == taskHostName)
+                    {
+                        exists++;
+                        var = th.Variables.Cast<Variable>().FirstOrDefault(x => x.Name == "vDestination");
+                        if (var != null)
+                        {
+                            exists++;
+                            if (var.EvaluateAsExpression)
+                            {
+                                if (var.Expression == "\" / \" +   @[$Project::dCatalogName]")
+                                {
+                                    var.Expression = "@[$Package::dDataSource] + \" / \" +   @[$Package::dCatalogName]";
+                                    exists++;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (exists == 3)
+                {
+                    //Console.WriteLine("Not Package:{0};{1}    {2}", p.Name, exists, var.Expression);
+                    Console.WriteLine("Not Package:{0}", p.Name);
+                    p.SaveToXML(out pText, null);
+                    File.WriteAllText(destPath + p.Name + ".dtsx", pText);
                 }
             }
         }
--- ConfigSSIS/SimpleDal.cs
+++ ConfigSSIS/SimpleDal.cs
@@ -19,13 +19,16 @@ namespace ConfigSSIS
             public string Name;
             public bool ToCopy;
             public bool ToStage;
+            public bool FullJoin;
+            public string PackageName;
+            public string PartnerSpec;
         }
         #endregion
 
         public static List<SyncTable> getSyncTables()
         {
             SqlConnection conn = new SqlConnection(connStr);
-            SqlCommand comm = new SqlCommand("select * from sync4partner.SyncTable where ToCopy=1", conn);
+            SqlCommand comm = new SqlCommand("select t.*,o.PackageName from sync4partner.SyncTable t join sync4partner.SyncTableOperational o on t.name=o.name where ToCopy=1", conn);
             DataTable table = new DataTable();
             comm.Connection.Open();
             table.Load(comm.ExecuteReader());
@@ -39,7 +42,10 @@ namespace ConfigSSIS
                 {
                     Name = r.Field<string>("Name"),
                     ToCopy = Convert.ToInt32(r["ToCopy"]) == 1,
-                    ToStage = Convert.ToInt32(r["ToStage"]) == 1
+                    ToStage = Convert.ToInt32(r["ToStage"]) == 1,
+                    PackageName = r.Field<string>("PackageName"),
+                    FullJoin = Convert.ToInt32(r["FullJoin"]) == 1,
+                    PartnerSpec = r.Field<string>("PartnerSpec")
                 };
                 lst.Add(aST);
             }
